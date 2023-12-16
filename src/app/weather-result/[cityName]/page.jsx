6203c6cfde6b7f41ae58fb33d5284ec6fe9693e9
emresarigul <emresarigul91@gmail.com>
2023-12-17 00:23:08 +0300

    fix

--- src/app/weather-result/[cityName]/page.jsx
+++ src/app/weather-result/[cityName]/page.jsx
@@ -1,6 +1,5 @@
 import SearchBox from "@/components/SearchBox";
 import WeatherResult from "@/components/WeatherResult";
-import Image from "next/image";
 import React from "react";
 
 export default async function page({ params }) {
