58eff8e6902cf0a0c50c0b3944700f151cc04d01
emresarigul <emresarigul91@gmail.com>
2024-02-10 04:20:19 +0300

    search box input fix

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -9,21 +9,32 @@ export default function SearchBox({ pageParam }) {
 
   const citySearch = (e) => {
     e.preventDefault();
-    if (!cityName) return;
-    router.push(`/weather-result/${cityName}`);
+    if (cityName.trim() === "") {
+      return;
+    }
+    router.push(`/weather-result/${cityName.trim()}`);
   };
+
+  console.log(cityName);
 
   return (
     <div className={pageParam}>
       <form onSubmit={citySearch} className="flex flex-col w-full relative">
         <input
+          onKeyDown={(e) => {
+            if (e.key === " " && cityName.trim() === "") {
+              e.preventDefault();
+            }
+          }}
           onChange={(e) => {
-            const fixedCityName = e.target.value.replace(/ã/g, "a");
-            setCityName(fixedCityName.trim());
+            const inputText = e.target.value;
+            const fixedCityName = inputText.replace(/ã/g, "a");
+            setCityName(fixedCityName);
           }}
           className="border-2 border-white rounded-lg outline-none h-14 lg:placeholder:text-xl placeholder:text-white bg-transparent px-5 text-white"
           type="text"
           placeholder="Search a city"
+          value={cityName}
         />
         <AiOutlineSearch
           className="text-2xl lg:text-3xl text-white absolute right-5 top-4 lg:top-3 cursor-pointer"
